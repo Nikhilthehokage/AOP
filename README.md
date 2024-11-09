78ed0120831f54556fbf6cbb124048cb91c6db3c
nikhilishi <smilenikhil99@gmail.com>
2024-11-09 20:19:41 +0530

    Pointcuts and wildcards

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Aspects configuration
-![image](https://github.com/user-attachments/assets/8a34b169-a549-42e9-8bd4-18902785c4e5)
-
+Write notes here
--- Spring-AOP/src/main/java/com/nikhil/aop/writingfirstaspect/aspect/LoggingAspect.java
+++ Spring-AOP/src/main/java/com/nikhil/aop/writingfirstaspect/aspect/LoggingAspect.java
@@ -2,12 +2,33 @@ package com.nikhil.aop.writingfirstaspect.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
+    /* Note - If execution(public String getName() is provided without class name it will execute for all classes which has this method.
+    *       wildcard -  * get*() - method starting with get and has zero arguments
+    *                   * get*(*) - method starting with get and has n arguments where n>0
+    *                   * get*(..) - method starting with get and has n arguments where n>=0
+     * */
+
+    //@Before("execution(public String com.nikhil.aop.writingfirstaspect.model.Circle.getName())")
+    /* To avoid redundancy in expressions point cuts are introduced*/
+    @Before("allGetters()")
     public void loggingAdvice(){
-        System.out.println("Advice run, advice executed successfully");
+        System.out.println("Advice run, advice one  executed successfully");
     }
+
+    @Before("allGetters()")
+    public void secondAdvice(){
+        System.out.println("Advice run, advice two executed successfully");
+    }
+
+    @Pointcut("execution(public String com.nikhil.aop.writingfirstaspect.model.Circle.getName())")
+    public void allGetters(){
+
+    }
+
+
 }
